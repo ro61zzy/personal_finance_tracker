3cf8b4d6e155ad8e79ba0be1a926070a28d7d5a8
rozzy <rosekaremeri@gmail.com>
2024-06-13 20:54:58 +0300

    correct file importation after updates

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-import useFinance from './useFinance';
+import useFinance from '../useFinance';
 
 const AccountCard = () => {
   const { data } = useFinance();
--- src/components/MyAccountsCard.jsx
+++ src/components/MyAccountsCard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Box, Typography } from '@mui/material';
-import useFinance from './useFinance';
+import useFinance from '../useFinance';
 
 const MyAccountsCard = () => {
   const { data } = useFinance();

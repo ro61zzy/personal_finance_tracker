143d85bb8391e513dbdbfcbe8728c9a736b6ca98
rozzy <rosekaremeri@gmail.com>
2024-06-13 20:44:07 +0300

    import data for account card

--- src/components/AccountCard.jsx
+++ src/components/AccountCard.jsx
@@ -1,27 +1,22 @@
-import { Box, Typography } from '@mui/material'
-import React from 'react'
+import React from 'react';
+import { Box, Typography } from '@mui/material';
+import useFinance from '../useFinance';
 
 const AccountCard = () => {
-    return (
-        <Box sx={{
-            p:"20px",
-            backgroundColor: "#f5f5f5",
-        }}>
-            <Typography sx={{
-                fontSize: "18px",
-                color: "grey"
-            }}>
-                Current Remaining Amount:
-            </Typography>
-            <Typography sx={{
-                fontSize: "25px",
-                color: "blue"
-            }}>
-                105,887
-            </Typography>
+  const { data } = useFinance();
 
-        </Box>
-    )
+  const totalAmount = Object.values(data.accounts).reduce((acc, curr) => acc + curr, 0);
+
+  return (
+    <Box sx={{ p: "20px", backgroundColor: "#f5f5f5" }}>
+      <Typography sx={{ fontSize: "18px", color: "grey" }}>
+        Current Remaining Amount:
+      </Typography>
+      <Typography sx={{ fontSize: "25px", color: "blue" }}>
+        {totalAmount}
+      </Typography>
+    </Box>
+  );
 }
 
-export default AccountCard
+export default AccountCard;
